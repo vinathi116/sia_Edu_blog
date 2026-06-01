7b678f95f10d9af99cd4182b56ed42d279fba517
Mahendra79 <janamahendra79@gmail.com>
2026-06-01 10:34:53 +0530

    increase time out

--- backend/render-start.sh
+++ backend/render-start.sh
@@ -8,4 +8,4 @@ if [ "${RUN_SEED_COURSES:-false}" = "true" ]; then
 fi
 
 python manage.py collectstatic --noinput
-exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000} --timeout 300
