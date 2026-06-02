19f775e68c7851bea374e8ccfe49456a5ff7d223
Mahendra79 <janamahendra79@gmail.com>
2026-06-02 14:50:37 +0530

    upadte images

--- frontend/src/data/courseImages.js
+++ frontend/src/data/courseImages.js
@@ -2,13 +2,13 @@ export const DEFAULT_COURSE_IMAGE_URL =
   "https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/971a437a-f346-4419-ae5a-3f0febd3a494.jpeg";
 
 export const COURSE_IMAGE_URLS_BY_ID = {
-  52: DEFAULT_COURSE_IMAGE_URL,
-  53: DEFAULT_COURSE_IMAGE_URL,
-  54: DEFAULT_COURSE_IMAGE_URL,
-  55: DEFAULT_COURSE_IMAGE_URL,
-  56: DEFAULT_COURSE_IMAGE_URL,
-  58: DEFAULT_COURSE_IMAGE_URL,
-  57: "https://cdn.corenexis.com/files/c/5133421720.png",
+  52: "https://cdn.corenexis.com/files/c/6651645720.png",
+  53: "https://cdn.corenexis.com/files/c/8776583720.png",
+  54: "https://cdn.corenexis.com/files/c/4223578720.png",
+  55: "https://cdn.corenexis.com/files/c/5482339720.png",
+  56: "https://cdn.corenexis.com/files/c/5181743720.png",
+  58: "https://cdn.corenexis.com/files/c/5133421720.png",
+  57: DEFAULT_COURSE_IMAGE_URL,
 };
 
 export function getCourseImageUrl(course) {
